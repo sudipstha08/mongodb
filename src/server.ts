a521dbeccade3dfd9ab71d074783ea5f59ac1548
sudipstha08 <sudipstha088@gmail.com>
2022-12-15 19:14:33 +0545

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import * as dotenv from 'dotenv' 
+import {IUser, User} from './models/user';
 dotenv.config()
 
 const app = express();
@@ -25,6 +26,20 @@ const subscribersRouter = require('./routes/subscribers')
 
 app.use('/subscribers', subscribersRouter)
 
+async function run() {
+  try {
+    await User.create({ name: "Kylee", email: "fsdfdsfsd@gmail.com"})
+    const user = await User.findOne({ name: "Kylee"}) as IUser
+    const Users = new User({})
+    // await User.findByName('Kyle')
+    user?.sayHi()
+  } catch (err: any) {
+    console.log("err: ", err.message)
+  }
+}
+
 app.listen(port, () => {
   return console.log(`<--------- LISTENING AT PORT: ${port}---------->`);
 });
+
+run()
